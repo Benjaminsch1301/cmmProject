59865bdd93dfd95dafb86fcca217ef284467c4be
Benjaminsch1301 <52360782+Benjaminsch1301@users.noreply.github.com>
2024-01-29 15:18:58 -0300

    uploading code and deleting plots

---
+++ utils/columns_transform.py
@@ -0,0 +1,70 @@
+minmax_scaler_features=[
+'Air Pressure at ground [hPa]', 'Air Pressure Normalised [hPa]',
+'Air Temperature at 30m [C]', 'Air Temperature at 2m [C]',
+'Air Temperature at ground [C]', 'Air Temperature below VLT [C]',
+'Dew Temperature at 30m [C]', 'Dew Temperature at 2m [C]',
+'Dew Temperature below VLT [C]', 'Rain intensity below VLT [%]',
+'Relative Humidity at 30m [%]', 'Relative Humidity at 2m [%]',
+'Relative Humidity below VLT [%]',
+'Wind Direction at 30m (0/360) [deg]',
+'Wind Direction at 30m (180/-180) [deg]',
+'Wind Direction at 10m (0/360) [deg]',
+'Wind Direction at 10m (180/-180) [deg]', 'Wind Speed at 30m [m/s]',
+'Wind Speed at 10m [m/s]', 'Wind Speed U at 20m [m/s]',
+'Wind Speed V at 20m [m/s]', 'Wind Speed W at 20m [m/s]',
+'Air Pressure instantanous [hPa]',
+'Air Temperature instantanous at 30m [C]',
+'Air Temperature instantanous at 2m [C]',
+'Air Temperature instantanous at ground [C]',
+'Air Temperature instantaneous below VLT [C]',
+'Dew Temperature istantaneous at 30m [C]',
+'Dew Temperature instantanous at 2m [C]',
+'Dew Temperature instantanous below VLT [C]',
+'Humidity instantanous at 30m [%]', 'Humidity instantanous at 2m [%]',
+'Humidity instantanous below VLT [%]',
+'Rain intensity instantanous [%]',
+'Wind Direction instantanous at 30m [deg]',
+'Wind Direction instantanous at 10m [deg]',
+'Wind Speed instantanous at 30m [m/s]',
+'Wind Speed instantanous at 10m [m/s]',
+'Wind Speed U instantanous at 20m [m/s]',
+'Wind Speed V instantanous at 20m [m/s]',
+'Wind Speed W instantanous at 20m [m/s]',
+]
+
+
+log_features = [
+    'Wind Speed W at 20m [m/s]'
+'Wind Speed instantanous at 30m [m/s]' # revisar outlier
+'Wind Speed instantanous at 10m [m/s]'
+    
+    
+]
+
+log3_features = [
+    'Air Pressure instantanous [hPa]'
+
+
+
+
+]
+
+
+log2_features = [
+    'Dew Temperature at 30m [C]',
+'Dew Temperature at 2m [C]',
+'Dew Temperature below VLT [C]',
+'Dew Temperature istantaneous at 30m [C]',
+'Dew Temperature instantanous at 2m [C]',
+'Dew Temperature instantanous below VLT [C]'
+]
+
+sqrt_features = [
+   'Wind Speed at 30m [m/s]',
+'Wind Speed at 10m [m/s]',
+'Wind Speed U at 20m [m/s]',
+'Wind Speed U instantanous at 20m [m/s]',
+'Wind Speed V at 20m [m/s]',
+'Wind Speed V instantanous at 20m [m/s]',
+'Wind Speed W instantanous at 20m [m/s]', 
+]
